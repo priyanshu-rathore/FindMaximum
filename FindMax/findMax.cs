80268d75c9eeab356b806f80b76590bb8933be2b
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-01-02 20:54:37 +0530

    Given 3 strings find the maximum

--- FindMax/findMax.cs
+++ FindMax/findMax.cs
@@ -8,7 +8,8 @@ namespace FindMaximum
         {
             int maxInt = findMaximum(1, 2, 3);
             float maxFloat = findMaximum(1.1f, 9.4f, 5.7f);
-
+            string maxString = findMaximum("Apple", "Peach", "Banana");
+            Console.WriteLine(maxString);
             Console.WriteLine(maxInt);
             Console.WriteLine(maxFloat);
 
@@ -30,6 +31,14 @@ namespace FindMaximum
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) return num1;
             else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) return num2;
             else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0) return num3;
+            else throw new Exception("Values are same");
+        }
+
+        static string findMaximum(string input1, string input2, string input3)
+        {
+            if (input1.CompareTo(input2) > 0 && input1.CompareTo(input3) > 0) return input1;
+            else if (input2.CompareTo(input1) > 0 && input2.CompareTo(input3) > 0) return input2;
+            else if (input3.CompareTo(input1) > 0 && input3.CompareTo(input2) > 0) return input3;
             else throw new Exception("Values are same");
         }
     }
